4051a978f7ba2aff1b485f96fb5023517bd81419
BartoszLewosz <bartosz.lewosz@gmail.com>
2020-11-15 17:54:21 +0000

    fix: remove space and colon from models

--- windows_cleaning/pages/models.py
+++ windows_cleaning/pages/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=60)
     permalink = models.CharField(max_length=12, unique=True)
-    update_date = models.DateTimeField('Last updated: ')
-    body_text = models.TextField('Page content: ', blank=True)
+    update_date = models.DateTimeField('Last updated')
+    body_text = models.TextField('Page content', blank=True)
