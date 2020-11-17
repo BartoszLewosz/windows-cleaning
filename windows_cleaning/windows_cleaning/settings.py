fb86ee417e2acca9e6f5b5e55584094c5d0c129b
BartoszLewosz <bartosz.lewosz@gmail.com>
2020-11-17 19:11:38 +0000

    fix:correct path to static files

--- windows_cleaning/windows_cleaning/settings.py
+++ windows_cleaning/windows_cleaning/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'windows_cleaning/static'),
 ]
