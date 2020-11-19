dabc1356c6af6589d329b5ade1b18d2c1ae5dba4
BartoszLewosz <bartosz.lewosz@gmail.com>
2020-11-19 20:47:17 +0000

    add all objects to views.py

--- windows_cleaning/pages/views.py
+++ windows_cleaning/pages/views.py
@@ -11,6 +11,7 @@ def index(request, pagename):
         'permalink': pg.permalink,
         'content': pg.body_text,
         'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
     }
     # assert False - uncomment to see error page and check local vars
     return render(request, 'pages/page.html', context)
