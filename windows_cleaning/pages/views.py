8029cd4db57faa5e5de5a7a1c864a99bb2b119c4
BartoszLewosz <bartosz.lewosz@gmail.com>
2020-11-21 16:23:18 +0000

    Create a view for contact form

--- windows_cleaning/pages/views.py
+++ windows_cleaning/pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from . models import Page
+from .forms import ContactForm
 
 def index(request, pagename):
     pagename = '/' + pagename
@@ -15,3 +16,19 @@ def index(request, pagename):
     }
     # assert False - uncomment to see error page and check local vars
     return render(request, 'pages/page.html', context)
+def contact(request):
+    submitted = False
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            #assert False
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'pages/contanct.html', {
+        'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
+    
