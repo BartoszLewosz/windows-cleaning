7ff6ddd07f0ae344837520ab81436f1368a4b6d9
BartoszLewosz <bartosz.lewosz@gmail.com>
2021-01-10 18:42:36 +0000

    Add label for django forms

--- windows_cleaning/pages/forms.py
+++ windows_cleaning/pages/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     your_name = forms.CharField(max_length=100, label='Your Name')
     email = forms.EmailField(required=False, label='Your e-mail address')
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
+    subject = forms.CharField(max_length=100, label='Subject')
+    message = forms.CharField(widget=forms.Textarea, label='Text area')
