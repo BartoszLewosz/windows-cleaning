6456e76c73c43df31b108140755bad47f088cc85
BartoszLewosz <bartosz.lewosz@gmail.com>
2021-01-10 19:18:20 +0000

    Style contact form

--- windows_cleaning/pages/forms.py
+++ windows_cleaning/pages/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    your_name = forms.CharField(max_length=100, label='Your Name')
-    email = forms.EmailField(required=False, label='Your e-mail address')
-    subject = forms.CharField(max_length=100, label='Subject')
-    message = forms.CharField(widget=forms.Textarea, label='Text area')
+    your_name = forms.CharField(max_length=100, label='NAME')
+    email = forms.EmailField(required=False, label='EMAIL')
+    phone = forms.CharField(max_length=15, label='PHONE')
+    subject = forms.CharField(max_length=100, label='SUBJECT')
+    message = forms.CharField(widget=forms.Textarea, label='MESSAGE')
