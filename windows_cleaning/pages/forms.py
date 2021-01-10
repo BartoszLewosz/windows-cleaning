3dbafe2f5e811d88bc931cba5fcac3fae03f238d
BartoszLewosz <bartosz.lewosz@gmail.com>
2021-01-10 20:10:02 +0000

    Style error messages in contact form

--- windows_cleaning/pages/forms.py
+++ windows_cleaning/pages/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    your_name = forms.CharField(max_length=100, label='NAME')
-    email = forms.EmailField(required=False, label='EMAIL')
-    phone = forms.CharField(max_length=15, label='PHONE')
-    subject = forms.CharField(max_length=100, label='SUBJECT')
-    message = forms.CharField(widget=forms.Textarea, label='MESSAGE')
+    your_name = forms.CharField(max_length=100, label='NAME', error_messages={'required': 'Please enter your name'})
+    email = forms.EmailField(label='EMAIL', error_messages={'required': 'Please enter your email'})
+    phone = forms.CharField(max_length=15, label='PHONE', error_messages={'required': 'Please enter your phone number'})
+    subject = forms.CharField(required=False, max_length=100, label='SUBJECT')
+    message = forms.CharField(widget=forms.Textarea, label='MESSAGE', error_messages={'required': 'Please enter your some details'})
