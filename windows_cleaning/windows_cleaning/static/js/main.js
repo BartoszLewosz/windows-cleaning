ac0ecca5f234a6ae126aaa694a45f6a1434b9fbc
BartoszLewosz <bartosz.lewosz@gmail.com>
2021-01-24 18:00:29 +0000

    Remove border-radius from focus on links

--- windows_cleaning/windows_cleaning/static/js/main.js
+++ windows_cleaning/windows_cleaning/static/js/main.js
@@ -1,7 +1,9 @@
 const burger = document.querySelector('.burger');
+const topNav = document.querySelector('.top-nav__list--js');
 
 const handleClick = () => {
     burger.classList.toggle('burger--active');
+    topNav.classList.toggle('top-nav__list--active');
 }
 
 burger.addEventListener('click', handleClick);
