fc94470ce78f262ff13d0e3a0c37f937bfd83f40
BartoszLewosz <bartosz.lewosz@gmail.com>
2021-01-25 00:40:35 +0000

    Animate the top-nav header

--- windows_cleaning/windows_cleaning/static/js/main.js
+++ windows_cleaning/windows_cleaning/static/js/main.js
@@ -1,9 +1,12 @@
 const burger = document.querySelector('.burger');
 const topNav = document.querySelector('.top-nav__list--js');
+const testJs = document.querySelector('.top-nav__header--js');
 
 const handleClick = () => {
+    testJs.classList.toggle('top-nav__header--hide');
     burger.classList.toggle('burger--active');
     topNav.classList.toggle('top-nav__list--active');
+    topNavHeader.toggle('top-nav__header--active');
 }
 
 burger.addEventListener('click', handleClick);
