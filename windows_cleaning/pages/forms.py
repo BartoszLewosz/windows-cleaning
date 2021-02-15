e8271e7ade212d8e9d516033063e69cb2c4bbfa3
bartosz <bartosz.lewosz@gmail.com>
2021-02-15 19:13:37 +0000

    Set sendgrid sandbox in settings

--- windows_cleaning/pages/forms.py
+++ windows_cleaning/pages/forms.py
@@ -4,5 +4,5 @@ class ContactForm(forms.Form):
     # your_name = forms.CharField(max_length=100, label='NAME', error_messages={'required': 'Please enter your name'})
     email = forms.EmailField(label='EMAIL', error_messages={'required': 'Please enter your email'})
     # phone = forms.CharField(max_length=15, required=False, label='PHONE', error_messages={'required': 'Please enter your phone number'})
-    # subject = forms.CharField(required=False, max_length=100, label='SUBJECT')
+    subject = forms.CharField(required=False, max_length=100, label='SUBJECT')
     message = forms.CharField(widget=forms.Textarea, label='MESSAGE', error_messages={'required': 'Please enter your some details'})
