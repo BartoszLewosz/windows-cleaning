3cb55f69debbedd92cc0c7aa6f0cc04960f7b691
bartosz <bartosz.lewosz@gmail.com>
2021-02-23 16:16:57 +0000

    Remove the old code

--- windows_cleaning/pages/forms.py
+++ windows_cleaning/pages/forms.py
@@ -6,8 +6,3 @@ class ContactForm(forms.Form):
     # phone = forms.CharField(max_length=15, required=False, label='PHONE', error_messages={'required': 'Please enter your phone number'})
     subject = forms.CharField(required=False, max_length=100, label='SUBJECT')
     message = forms.CharField(widget=forms.Textarea, label='MESSAGE', error_messages={'required': 'Please enter some details'})
-
-class ContactFormTwo(forms.Form):
-    name = forms.CharField(max_length=80)
-    msg = forms.CharField(widget=forms.Textarea)
-    e_mail = forms.EmailField()
